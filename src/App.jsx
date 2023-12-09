d9c4a732c13b9f67fa8cc2cecd483e2987af7000
Luke B <142310217+OGOZ111@users.noreply.github.com>
2023-12-09 11:30:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { useState } from 'react';
+import { useMemo } from 'react';
+
+// NON RECURSIVE METHOD, USING BUILT IN JS FUNCTIONS
+
+
+function App() {
+
+  const [word, setWord] = useState("");
+  const PalinCheck = useMemo(() => {
+    return word === word.split("").reverse().join("");
+  }, [word]);
+
+
+  return (
+    <>
+
+      <form>
+        <div>
+          <label>Type in Word</label>
+          <input value={word} onChange={(e) => setWord(e.target.value)} />
+        </div>
+      </form>
+      <div>Is Palindrome:{PalinCheck ? "Yes" : "No"}</div>
+
+    </>
+  )
+}
+
+export default App
+
